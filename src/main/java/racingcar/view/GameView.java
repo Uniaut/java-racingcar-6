1c0183db3d9bc768259f349368ee06cc308949e5
Uniaut <kunwoo0927@gmail.com>
2023-11-05 00:45:00 +0900

    refactor(GameView): declare asking text into constant
    GameView:
        Asking for car names -> ASKING_CAR_NAMES_TEXT
        Asking for number attempts -> ASKING_NUMBER_ATTEMPTS_TEXT

--- src/main/java/racingcar/view/GameView.java
+++ src/main/java/racingcar/view/GameView.java
@@ -4,18 +4,20 @@ import camp.nextstep.edu.missionutils.Console;
 import racingcar.dto.ConsoleInput;
 
 public class GameView {
+    private static final String ASKING_CAR_NAMES_TEXT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ASKING_NUMBER_ATTEMPTS_TEXT = "시도할 회수는 몇회인가요?";
 
     public GameView() {
     }
 
     public ConsoleInput askCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(ASKING_CAR_NAMES_TEXT);
         String input = Console.readLine();
         return new ConsoleInput(input);
     }
 
     public ConsoleInput askNumberAttempts() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(ASKING_NUMBER_ATTEMPTS_TEXT);
         String input = Console.readLine();
         return new ConsoleInput(input);
     }
